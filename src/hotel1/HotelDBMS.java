b8b90fbf0ea9048dcee5f834a1a693bd3c045cdb
dhikra909 <alm@ka909@gmail.com>
2023-01-02 04:55:21 -0800

    insrt

--- src/hotel1/HotelDBMS.java
+++ src/hotel1/HotelDBMS.java
@@ -19,6 +19,21 @@ public class HotelDBMS {
 				+" updated_date date,"
 				+" is_Active BIT not NULL, "
 				+ " PRIMARY KEY ( id ))";
+		
+		String in = "INSERT INTO Hotels VALUES(1,'KHU','JAH','1996-11-18','1997-06-17',true),"
+				 + "(2,'KHA','JAH','1998-07-27','1997-01-11',true),"
+				 +  "(3,'ARE','YAH','2000-12-07','1997-02-12',true),"
+				 + "(4,'AHM','YAH','1996-05-25','1997-03-13',true),"
+				 + "(5,'AMM','JAB','2001-06-15','1997-04-14',true),"
+				 + "(6,'RIT','JAB','2005-05-05','1997-05-15',true),"
+				 + "(7,'JAB','JAB','1997-08-02','1997-06-16',true),"
+				 + "(8,'SAL','HAM','1996-05-25','1997-07-18',true),"
+				 + "(9,'SAHE','HAM','1996-05-25','1997-08-19',true),"
+				 + "(10,'FAH','HAM','1998-05-05','1997-09-20',true)," 
+				 + "(11,'HAM','BAR','2007-09-10','1997-10-22',true),"
+				 + "(12,'HAM','BAR','2005-08-14','1997-11-23',true)";
+		
+		
 		Connection conn = null;
 		try {
 			Driver driver = (Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
@@ -26,12 +41,22 @@ public class HotelDBMS {
 			conn = DriverManager.getConnection(url, user, pass);
 			Statement st = (Statement) conn.createStatement();
 		
-			try{	
-				st.executeUpdate(sqlDB);
-				   System.out.println("Table Created Successfully");   	 
-			      } catch (SQLException e) {
-			         e.printStackTrace();
-			      }
+			int m = st.executeUpdate(sqlDB);
+			int s= st.executeUpdate(in);
+			if ((m >= 0)&& (s >= 1)){
+				System.out.println("Created table in given database...");
+				System.out.println("Inserted to table SUCC...");
+				return true;
+			} else {
+				System.out.println(" table already Created in given database...");
+				System.out.println(" INSERTED DATA TO TABLE...");
+			}
+//			try{	
+//				st.executeUpdate(sqlDB);
+//				   System.out.println("Table Created Successfully");   	 
+//			      } catch (SQLException e) {
+//			         e.printStackTrace();
+//			      }
 		
 			conn.close();
 		}
--- src/hotel1/Main.java
+++ src/hotel1/Main.java
@@ -1,5 +1,7 @@
 package hotel1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -24,6 +26,40 @@ public class Main {
 		Employees Employees = new Employees();
 		Employees.addingEmployeesTable();
 		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Select one of the options:");
+			System.out.println("1. Insert 10,000 hotels");
+			System.out.println("2. Insert 1 hote");
+			System.out.println("3. Print 10 hotels");
+			System.out.println("4. Make first 10 hotels 'is_Active' = false");
+			System.out.println("5. Print hotel information by user input");
+			int select = sc.nextInt();
+		
+			switch(select) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:	
+		
+				break;
+			case 4:	
+				
+				break;
+			case 5:	
+				
+				break;
+			
+			}		
+		
+		
+		
+		
+		
 	}
+	
+
 
 }
